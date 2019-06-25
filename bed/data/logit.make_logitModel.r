ccc925f9774246f442b4bb582c10d340aa47b7e2
niujeffrey <devnull@localhost>
2019-06-25 12:19:02 -0400

    tools added to config

---
+++ bed/data/logit.make_logitModel.r
@@ -0,0 +1,4 @@
+data <- read.table(file="data/logit.feature.xls",sep="\t",header=T)
+attach(data)
+mylogit <- glm(Label ~ mRNA + ORF + Fickett + Hexamer, family=binomial(link="logit"), na.action=na.pass)
+save.image("data/logit.logit.RData")
