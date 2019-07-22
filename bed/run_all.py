a39e7187ad2db2eac5914ae2f4feb7431bf0b801
niujeffrey <devnull@localhost>
2019-07-22 13:51:08 -0400

    cleaning up code

--- bed/run_all.py
+++ bed/run_all.py
@@ -1,4 +1,9 @@
-# Runs all the tools in config.yaml
+"""
+Runs the tools in config.yaml and tests whether they can properly
+parse the file format.
+
+Outputs the results to a binary file to be visualized using combine.py
+"""
 import os
 import sys
 import subprocess
@@ -16,6 +21,9 @@ except ImportError:
 
 
 def get_file_names():
+    """
+    Gets a list of the names of the test files that correspond to the list of results
+    """
     file_names = []
     for directory in os.listdir("./good/"):
         for file in os.listdir("./good/" + directory):
@@ -36,6 +44,15 @@ def get_file_names():
 
 
 def run_all(output_file, verbose=False, failed_good_file="out/failed_good.txt", passed_bad_file="out/passed_bad.txt"):
+    """
+    Calls run_bad or run_good from run_utils.py to run tools against the test suite.
+    Outputs the results to <output_file> as a binary file.
+
+    output_file: the binary file containing the results
+    verbose: if verbose is True, more information will be printed to the screen
+    failed_good_file: the output text file containing the outputs from the tools where it failed on a good test case
+    passed_bad_file: the output text file containing the outputs from the tools where it passed on a bad test case
+    """
     # Clear the previous data and reinitalize it
     subprocess.call(["rm", "-f", failed_good_file, passed_bad_file])
     subprocess.call(["touch", failed_good_file, passed_bad_file])
@@ -47,8 +64,8 @@ def run_all(output_file, verbose=False, failed_good_file="out/failed_good.txt", 
     stream = open('config.yaml', 'r')
     data = load(stream, Loader=Loader)
 
-    command_insertions = data['settings']['file-locations']
-    python_versions = data['python-versions']
+    command_insertions = data['settings']['file-locations']  # Locations of "constant" files
+    python_versions = data['python-versions']  # Each tool with its corresponding Python version
 
     correct_list = []
     name_list = []
@@ -56,55 +73,58 @@ def run_all(output_file, verbose=False, failed_good_file="out/failed_good.txt", 
     tool_list = data['tools']
     for tool in tool_list:
         for program in list(tool.keys()):
-            if program != 'ucsc': continue
-            if python_versions[program] != version:
+            if python_versions[program] != version:  # Skip the tool if the wrong Python version is present
                 continue
 
             commands = tool[program]
             
             for command, execution in commands.items():
-                current_array = []
+                current_array = []  # Array of how the program performed on each test case. 0 = incorrect, 1 = correct
                 title = program + " " + command
                 name_list.append(title)
 
-                m = (88 - len(title)) // 2
+                m = (88 - len(title)) // 2  # m,n for aesthetic purposes only
                 n = m if len(title) % 2 == 0 else m + 1
 
                 print("*"*m + " " + title + " " + "*"*n)
                 print("*"*33 + " strict good test cases " + "*"*33)
-                current_array.extend(run_good(execution, "./good/", title, verbose, failed_good_file, command_insertions))
+                current_array.extend(
+                    run_good(execution, "./good/", title, verbose, failed_good_file, command_insertions))
                 print("*"*90)
                 print()
-                current_array.append(0.5)
+                current_array.append(0.5)  # 0.5 is a separator for heatmap purposes
 
                 print("*"*33 + " non-strict good cases " + "*"*34)
-                current_array.extend(run_good(execution, "./less_good/", title, verbose, failed_good_file, command_insertions))
+                current_array.extend(
+                    run_good(execution, "./less_good/", title, verbose, failed_good_file, command_insertions))
                 print("*"*90)
                 print()
                 current_array.extend([0.5, 0.5, 0.5])
 
                 print("*"*31 + " non-strict bad test cases " + "*"*32)
-                current_array.extend(run_bad(execution, "./less_bad/", title, verbose, failed_good_file, command_insertions))
+                current_array.extend(
+                    run_bad(execution, "./less_bad/", title, verbose, passed_bad_file, command_insertions))
                 print("*"*90)
                 print()
                 current_array.append(0.5)
 
                 print("*"*33 + " strict bad test cases " + "*"*34)
-                current_array.extend(run_bad(execution, "./bad/", program + " " + command, verbose, passed_bad_file, command_insertions))
+                current_array.extend(
+                    run_bad(execution, "./bad/", program + " " + command, verbose, passed_bad_file, command_insertions))
                 print("*"*90)
                 print()
                 correct_list.append(current_array)
-            
-    stream.close()
 
-    num_correct = [l.count(1) for l in correct_list]
+    num_correct = [l.count(1) for l in correct_list]  # Used for sorting purposes in combine.py
     
-    with open(output_file, 'wb') as fp:
+    with open(output_file, 'wb') as fp:  # Dump the results of these set of tools into a binary file
         pickle.dump([num_correct, correct_list, name_list], fp)
+
+    stream.close()
 
 
 def usage():
-    print(
+    sys.stderr.write(
     """Tester for the BED format. Tests the tools in config.yaml to see if they appropriately throw warnings or errors.
 Usage: run_all.py -o [-h] [-v] [-V] [--failed-good=] [--passed-bad=]
 
@@ -118,7 +138,7 @@ options:
                   Default: out/failed_good.txt
     --passed-bad=  The output file that logs cases where bad files ran without error or warning
                   Default: out/passed_bad.txt
-    """, file=sys.stderr)
+""")
 
 
 if __name__ == '__main__':
@@ -154,7 +174,7 @@ if __name__ == '__main__':
             assert False, "unhandled option"
     
     if output_file is None:
-        print("Missing required argument -o\n", file=sys.stderr)
+        sys.stderr.write("Missing required argument -o\n")
         usage()
         exit(2)
     
