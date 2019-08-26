1a608aa5a7b8fbb21c3b2619b3d434c97a6a9cdc
niujeffrey <devnull@localhost>
2019-08-26 10:37:37 -0400

    work

--- bed/summarize.py
+++ bed/summarize.py
@@ -12,7 +12,7 @@ import numpy as np
 
 from more_itertools import sort_together
 
-import seaborn as sns; sns.set(font_scale=2.1)
+import seaborn as sns;
 
 BED_NAMES = ['BED3', 'BED4', 'BED5', 'BED6', 'BED7', 'BED8', 'BED9', 'BED10',
              'BED11', 'BED12']
@@ -28,8 +28,13 @@ if __name__ == '__main__':
         help="full filepath to output image file containing" +
         " the heatmap. (eps, pdf, png, raw, rgba, svg, jpg," +
         " jpeg, tif, tiff)")
-    parser.add_argument('--flatten', action='store_true', help="Combine subtools together")
+    parser.add_argument('--expand', action='store_true',
+                        help="Do not combine subtools together")
+    parser.add_argument('--mini', action='store_true',
+                        help="Make plot smaller")
     args = parser.parse_args()
+
+    sns.set(font_scale=0.6) if args.mini else sns.set(font_scale=2.3)
 
     files = []
     for f in args.results_file:
@@ -53,7 +58,7 @@ if __name__ == '__main__':
             correct_list = l[1]
             name_list = l[2]
             for i in range(len(num_correct)):
-                if args.flatten:
+                if args.expand:
                     name = name_list[i]
                     if name not in max_bed_correctness:
                         max_bed_correctness[name] = {}
@@ -78,7 +83,7 @@ if __name__ == '__main__':
                         min_bed_correctness[name][len(correct_list[i])] = \
                             num_correct[i] / (len(correct_list[i]) - 1)
 
-    if args.flatten:
+    if args.expand:
         min_bed_correctness = max_bed_correctness
 
     programs = list(max_bed_correctness.keys())
@@ -103,16 +108,21 @@ if __name__ == '__main__':
         [programs, heatmap_array, display_array, sorting_temp], key_list=[3]
     )
 
-    annot = True if args.flatten else np.array(display_array, dtype=object)
-    fmt = '.2g' if args.flatten else ''
-    plt.figure(figsize=(39, 50))
+    annot = True if args.expand else np.array(display_array, dtype=object)
+    fmt = '.2g' if args.expand else ''
 
-    sizing = {'fontsize': 48}
+    plt.figure(figsize=(10.5,13)) if args.mini else plt.figure(figsize=(39, 50))
+
+    sizing = {'fontsize': 12} if args.mini else {'fontsize': 48}
     ax = sns.heatmap(heatmap_array, cmap=plt.get_cmap('bwr'),
         vmin=0, vmax=1, square=False, linewidths=.5, xticklabels=BED_NAMES,
         yticklabels=programs, annot=annot, cbar=True, fmt=fmt,
         cbar_kws={"fraction": 0.03, "pad": 0.01}
     )
+    # ax = sns.heatmap(heatmap_array, cmap=plt.get_cmap('bwr'),
+    #     vmin=0, vmax=1, linewidths=0.3, xticklabels=BED_NAMES,
+    #     yticklabels=False, cbar=True, cbar_kws={'fraction': 0.03, 'pad': 0.01}
+    # )
     
     ax.set_ylabel('Tools', **sizing)
     ax.set_xlabel('Bed types', **sizing)
